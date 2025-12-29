fa4e3ce58d487655a577bb017f69b8595a0a18fc
irealyash7 <yscreations636@gmail.com>
2025-12-29 09:05:50 -0800

    Created signup functionality

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,17 @@
 import express from "express"
 import router from "./routes/auth.js"
+import dotenv from "dotenv"
+import {connectDB} from "./lib/db.js"
+dotenv.config()
 const app=express();
+
+app.use(express.json())
 
 app.use("/api/auth",router)
 
 
 
-app.listen(5001,()=>{
-    console.log("Server is running on port 5001")
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port `+ process.env.PORT)
+    connectDB()
 })
